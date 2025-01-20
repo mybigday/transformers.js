5da635acdd10f9bb050c8b9b0d4f45e7e16b69ec
Hans <hans.chen@bricks.tools>
2025-01-20 20:09:20 +0800

    Fix test

--- jest.config.mjs
+++ jest.config.mjs
@@ -86,7 +86,9 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    'native-universal-fs': 'node:fs/promises',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
