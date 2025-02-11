8f4b0e30eb81e1a8c7fe7e016703f30505165b4c
Hans <hans.chen@bricks.tools>
2025-02-11 14:41:25 +0800

    Manually mock `react-native` for test

--- jest.config.mjs
+++ jest.config.mjs
@@ -88,6 +88,7 @@ export default {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     'native-universal-fs': 'node:fs/promises',
+    'react-native': new URL('./tests/react-native.mock.js', import.meta.url).pathname,
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
